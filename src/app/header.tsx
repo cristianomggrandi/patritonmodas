4a47a6c459146181efe35b37f9043dc9448ad04e
Cristiano Grandi <80713582+cristianomggrandi@users.noreply.github.com>
2024-01-26 17:27:40 -0300

    Alterar logo e cores

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -6,18 +6,21 @@ import { IoIosArrowForward } from "react-icons/io"
 export default function Header() {
     return (
         <header>
-            <nav className="bg-black text-white">
+            <nav className="bg-white text-primary">
                 <ul className="flex justify-around">
                     <li className="flex flex-1 items-center justify-center">
-                        <input className="rounded-full p-2" placeholder="O que você procura?" />
+                        <input
+                            className="border-primary border-2 text-white rounded-full p-2"
+                            placeholder="O que você procura?"
+                        />
                     </li>
                     <li className="flex flex-1 items-center justify-center">
                         <Link href="/">
                             <Image
                                 src="/logopatritonmodas.jpg"
                                 alt="Logo Patriton Modas"
-                                width={135}
-                                height={81}
+                                width={192}
+                                height={72}
                             />
                         </Link>
                     </li>
@@ -27,15 +30,15 @@ export default function Header() {
                 </ul>
             </nav>
             <nav>
-                <ul className="flex justify-around bg-black text-white p-2 list-none">
+                <ul className="flex justify-around bg-primary text-white p-2 list-none">
                     <li className="flex-1 text-center relative justify-center hover:border-b-2 hover:-mb-1 border-solid border-cement">
                         <Link className="block w-full" href="/novidades">
                             Novidades
                         </Link>
                     </li>
                     <li className="flex-1 text-center relative justify-center hover:border-b-2 hover:-mb-1 border-solid border-cement">
-                        <Link className="block w-full" href="/promocao">
-                            Promoção
+                        <Link className="block w-full" href="/promocoes">
+                            Promoções
                         </Link>
                     </li>
                     <li className="flex-1 text-center relative hover:border-b-2 hover:-mb-1 border-solid border-cement dropdown-categorias">
@@ -44,38 +47,38 @@ export default function Header() {
                             <IoIosArrowForward className="dropdown-arrow inline-block ml-3" />
                         </Link>
                         <div className="absolute hidden w-full">
-                            <ul className="w-full p-2 bg-black flex flex-col">
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                            <ul className="w-full p-2 bg-primary flex flex-col">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/vestido">
                                         Vestido
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/shorts">
                                         Shorts
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/saias">
                                         Saias
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/casacos">
                                         Casacos
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/jeans">
                                         Jeans
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/macacao">
                                         Macacão
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/calcados">
                                         Calçados
                                     </Link>
@@ -89,38 +92,38 @@ export default function Header() {
                             <IoIosArrowForward className="dropdown-arrow inline-block ml-3" />
                         </Link>
                         <div className="absolute hidden w-full">
-                            <ul className="w-full p-2 bg-black flex flex-col gap-2">
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                            <ul className="w-full p-2 bg-primary flex flex-col">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/vestido">
                                         Vestido
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/shorts">
                                         Shorts
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/saias">
                                         Saias
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/casacos">
                                         Casacos
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/jeans">
                                         Jeans
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/macacao">
                                         Macacão
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/calcados">
                                         Calçados
                                     </Link>
@@ -134,38 +137,38 @@ export default function Header() {
                             <IoIosArrowForward className="dropdown-arrow inline-block ml-3" />
                         </Link>
                         <div className="absolute hidden w-full">
-                            <ul className="w-full p-2 bg-black flex flex-col gap-2">
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                            <ul className="w-full p-2 bg-primary flex flex-col">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/vestido">
                                         Vestido
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/shorts">
                                         Shorts
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/saias">
                                         Saias
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/casacos">
                                         Casacos
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/jeans">
                                         Jeans
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/macacao">
                                         Macacão
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/calcados">
                                         Calçados
                                     </Link>
@@ -179,23 +182,23 @@ export default function Header() {
                             <IoIosArrowForward className="dropdown-arrow inline-block ml-3" />
                         </Link>
                         <div className="absolute hidden w-full">
-                            <ul className="w-full p-2 bg-black flex flex-col gap-2">
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                            <ul className="w-full p-2 bg-primary flex flex-col">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/vestido">
                                         Vestido
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/shorts">
                                         Shorts
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/macacao">
                                         Macacão
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/calcados">
                                         Calçados
                                     </Link>
@@ -209,18 +212,18 @@ export default function Header() {
                             <IoIosArrowForward className="dropdown-arrow inline-block ml-3" />
                         </Link>
                         <div className="absolute hidden w-full">
-                            <ul className="w-full p-2 bg-black flex flex-col gap-2">
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                            <ul className="w-full p-2 bg-primary flex flex-col">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/saias">
                                         Saias
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/casacos">
                                         Casacos
                                     </Link>
                                 </li>
-                                <li className="flex items-center border-b-2 border-solid border-cement">
+                                <li className="flex items-center border-b-2 border-solid border-white">
                                     <Link className="w-full p-1 text-left" href="/jeans">
                                         Jeans
                                     </Link>
