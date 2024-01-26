dff564ddc92b600b330d5937c417c0b34e103274
Cristiano Grandi <80713582+cristianomggrandi@users.noreply.github.com>
2024-01-26 19:38:08 -0300

    Ajustar tamanho e proporção de imagens de carrossel

--- src/app/carousel.tsx
+++ src/app/carousel.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react"
 export default function Carousel() {
     const [imageIndex, setImageIndex] = useState(0)
 
-    const imageArray = ["/slideexemplo.jpg", "/slideexemplo2.jpg"]
+    const imageArray = ["/slide.jpeg", "/slideexemplo.jpg", "/slideexemplo2.jpg"]
 
     useEffect(() => {
         const imageChangeInterval = setInterval(() => {
@@ -16,18 +16,28 @@ export default function Carousel() {
         return () => clearInterval(imageChangeInterval)
     })
 
+    const proportions = {
+        x: 939,
+        y: 372,
+    }
+
     return (
-        <div className="w-[672px] h-[378px] overflow-hidden">
+        <div className="w-full overflow-hidden">
             <div className="w-full h-full">
                 {imageArray.map((imageSrc, index) => (
                     <div
                         key={index}
-                        className="absolute transition-opacity duration-700"
+                        className="w-full absolute flex justify-center transition-opacity duration-700"
                         style={{
                             opacity: imageIndex === index ? 100 : 0,
                         }}
                     >
-                        <Image width={672} height={378} src={imageSrc} alt="Slide de exemplo" />
+                        <Image
+                            width={proportions.x}
+                            height={proportions.y}
+                            src={imageSrc}
+                            alt="Slide de exemplo"
+                        />
                     </div>
                 ))}
             </div>
