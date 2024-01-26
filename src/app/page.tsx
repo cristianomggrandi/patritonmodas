45f3aa7c1e28247ec34023821336bb92e394ad63
Cristiano Grandi <80713582+cristianomggrandi@users.noreply.github.com>
2024-01-26 19:43:56 -0300

    Remover padding de página principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Carousel from "./carousel"
 export default function Home() {
     return (
         <main
-            className="flex min-h-screen flex-col items-center justify-between p-5 bg-white text-black"
+            className="flex min-h-screen flex-col items-center justify-between bg-white text-black"
             // TODO: Ver se bg-beige é melhor
         >
             <Carousel />
